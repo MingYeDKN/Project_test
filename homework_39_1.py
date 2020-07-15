e5d14e4c48ba6df83a72219c0edf41125e6d9da7
MingYe <mingye99520@gmail.com>
2020-07-15 19:35:39 +0800

    change the python file and License

--- homework_39_1.py
+++ homework_39_1.py
@@ -30,7 +30,14 @@ class Stack:
             print(self.stack[-1])
             return self.stack[-1]
 
-
+print("修改")
+start2 = [-1, 0, 1]
+stack2 = Stack(start)
+stack2.push(2)
+stack2.bottom()
+stack2.top()
+stack2.pop()
+print(stack2.isEmpty())
 start = [-1, 0, 1]
 stack1 = Stack(start)
 stack1.push(2)
